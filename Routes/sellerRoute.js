8f9e96d3304e3c2ec71dc1c801971ee65d68757a
mahiuddiinnTuhin <mahiuddiinntuhin@gmail.com>
2023-04-10 13:35:51 +0600

    seller route

--- Routes/sellerRoute.js
+++ Routes/sellerRoute.js
@@ -7,6 +7,7 @@ const { default: mongoose } = require("mongoose");
 const SellerProduct = new mongoose.model("SellerProduct", product);
 
 SellerRoute.use(express.json());
+
 const verifyToken = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
