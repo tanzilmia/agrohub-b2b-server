24c5b121af03501b1ed6bfc71a07d076c8b89e38
Istiakahmed180 <istiakahmed180@gmail.com>
2023-04-11 00:45:42 +0600

    seller product sell complete

--- Scema/SellerScema/PostSellerScema.js
+++ Scema/SellerScema/PostSellerScema.js
@@ -1,23 +1,83 @@
 const { default: mongoose } = require("mongoose");
 
 const postSellerScema = mongoose.Schema({
-  sellerName: String,
-  sellerEmail: String,
-  sellerPhone: String,
-  sellerProfilePicture: String,
-  name: String,
-  image: String,
-  oldPrice: String,
-  newPrice: String,
-  size: Array,
-  stock: String,
-  description: String,
-  role: String,
-  additionalInfo: Array,
+  sellerName: {
+    type: String,
+    required: [true, "Please Enter Seller Name"],
+    trim: true,
+  },
+  sellerEmail: {
+    type: String,
+    required: [true, "Please Enter Seller Email"],
+    trim: true,
+  },
+  sellerPhone: {
+    type: String,
+    required: [true, "Please Enter Seller Phone"],
+    trim: true,
+  },
+  sellerProfilePicture: {
+    type: String,
+    required: [true, "Please Enter Seller Profile Picture"],
+    trim: true,
+  },
+  name: {
+    type: String,
+  },
+  images: {
+    type: Array,
+    required: [true, "please Enter Product Images"],
+  },
+  oldPrice: {
+    type: Number,
+    required: [true, "Please Enter Product Price"],
+    maxLength: [8, "Price Cannot Exceed 8 Characters"],
+  },
+  newPrice: {
+    type: Number,
+    required: [true, "Please Enter Product Price"],
+    maxLength: [8, "Price Cannot Exceed 8 Characters"],
+  },
+  size: {
+    type: Array,
+    required: [true, "Please Enter Product Size"],
+  },
+  stock: {
+    type: String,
+    required: [true, "Please Enter Product Stock"],
+  },
+  rating: {
+    type: Number,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter Product Description"],
+  },
+  role: {
+    type: String,
+    required: [true, "please Enter Seller Role"],
+  },
+  additionalInfo: {
+    type: Array,
+    required: [true, "please Enter Additional Information"],
+  },
+  totalSells: {
+    type: Number,
+  },
+  totalQuantity: {
+    type: Number,
+  },
+  category: {
+    type: String,
+    required: [true, "please Enter Product Category"],
+  },
+  brand: {
+    type: String,
+    required: [true, "please Enter Product Brand"],
+  },
 });
 
 module.exports = postSellerScema;
-
 
 // sellerName: {
 //   type: String,
@@ -62,11 +122,10 @@ module.exports = postSellerScema;
 //   required: [true, "Please Enter product Price"],
 // },
 
-
 // category: {
 //   type: String,
 //   required: [true, "please Enter Your Brand Name"],
-  
+
 // },
 // size: [
 //   {
