c86d111d2ca8a84a5e89bb9439f36d3e608af0e4
Md tanzil <ziltan590@gmail.com>
2023-04-16 15:38:44 +0600

    chat app api

--- Routes/chatRoute.js
+++ Routes/chatRoute.js
@@ -6,7 +6,7 @@ const userscema = require("../Scema/users");
 const { default: mongoose } = require("mongoose");
 const User = new mongoose.model("User", userscema);
 const Chat = require("../Model/chatModel");
-
+const Message = require("../Model/messageModel");
 
 const verifyToken = async (req, res, next) => {
   try {
@@ -30,8 +30,6 @@ const verifyToken = async (req, res, next) => {
   }
 };
 
-
-
 ChatRoute.get("/", (req, res) => {
   res.send("Chat route is running");
 });
@@ -54,9 +52,12 @@ ChatRoute.get("/search-seller", async (req, res) => {
   } catch (e) {}
 });
 
+// access chats
 
 ChatRoute.post("/accessChat", verifyToken, async (req, res) => {
   try {
+    let chatData; // Declare the variable here
+
     const { userId } = req.body;
 
     if (!userId) {
@@ -70,19 +71,25 @@ ChatRoute.post("/accessChat", verifyToken, async (req, res) => {
     })
       .populate("users", "-password")
       .populate("latestMessage")
-      .populate({ path: "latestMessage.sender", select: "name profilePic email" });
+      .populate({
+        path: "latestMessage.sender",
+        select: "name profilePic email",
+      });
 
     if (isChat.length > 0) {
       return res.send(isChat[0]);
     } else {
-      const chatData = {
+      chatData = {
         chatName: "sender",
         isGroupChat: false,
         users: [req.user._id, userId],
       };
 
       const createdChat = await Chat.create(chatData);
-      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password");
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
 
       return res.send(fullChat);
     }
@@ -92,22 +99,70 @@ ChatRoute.post("/accessChat", verifyToken, async (req, res) => {
   }
 });
 
+// fetch chats
+ChatRoute.get("/fetchChat", verifyToken, async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (result) => {
+        result = await User.populate(result, {
+          path: "latestMessage.sender",
+          select: "name profilePic email",
+        });
+        res.status(200).send(result);
+      });
+  } catch (e) {
+    res.send(e.message);
+  }
+});
 
-ChatRoute.get("/fetchChat", async (req, res) => {
-  res.send("Chat route is running");
+ChatRoute.post("/sendMessage", verifyToken, async (req, res) => {
+  try {
+    const { content, chatId } = req.body;
+    if (!content || !chatId) {
+      console.log("invalid data pass into request");
+      return res.sendStatus(400);
+    }
+    const newMessage = {
+      sender: req.user._id,
+      content: content,
+      chat: chatId,
+    };
+    let message = await Message.create(newMessage);
+    message = await message.populate("sender", "name profilePic");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name profilePic email",
+    });
+    await Chat.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    });
+    res.send(message);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
-ChatRoute.post("/create-groupChat", async (req, res) => {
-  res.send("Chat route is running");
+
+
+// get message
+
+ChatRoute.get("/:chatId", verifyToken, async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name profilePic email")
+      .populate("chat");
+
+      res.send(messages)
+  } catch (e) {
+    res.send(e.message);
+  }
 });
-ChatRoute.put("/nenameGroup", async (req, res) => {
-  res.send("Chat route is running");
-});
-ChatRoute.put("/remove-fromGrop", async (req, res) => {
-  res.send("Chat route is running");
-});
-ChatRoute.put("/addto-Group", async (req, res) => {
-  res.send("Chat route is running");
-});
+
+
 
 ChatRoute.get("/");
 
